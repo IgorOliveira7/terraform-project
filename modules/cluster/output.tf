85be324366c593b8b2b77c3fcdab021796c3819f
igor <igor@igor.myguest.virtualbox.org>
2025-09-03 22:01:00 -0300

    feat: added controller module and adjustments in cluster and network modules

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,11 +1,15 @@
-output "eks_vpc_config" {
-  value = aws_eks_cluster.eks_cluster.vpc_config
+output "cluster_name" {
+  value = aws_eks_cluster.eks_cluster.id
 }
 
 output "oidc" {
-  value = data.tls_certificate.eks_oidc_tls_certificate.certificates[*].sha1_fingerprint
+  value = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
-output "cluster_name" {
-  value = aws_eks_cluster.eks_cluster.id
+output "certificate_authority" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks_cluster.endpoint
 }
